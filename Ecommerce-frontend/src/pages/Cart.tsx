5e3883867cc374f9200647f17700d2cc828475ab
Aryaman3007 <Aryaman3007>
2024-04-25 00:09:49 +0530

    Project started with the Admin dashboard

---
+++ Ecommerce-frontend/src/pages/Cart.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div>
+      <text>hii</text>
+    </div>
+  )
+}
+
+export default Cart
---
+++ Ecommerce-frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <text>hii</text>
+    </div>
+  )
+}
+
+export default Home
